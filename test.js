705d9d21ad919377729896479439fc658ea49400
George Lifchits <george.lifchits@gmail.com>
2017-09-13 22:03:05 -0400

    add test for simple feature properties

--- test.js
+++ test.js
@@ -45,6 +45,52 @@ test('simple test', t => {
 
 })
 
+
+test('test simple properties', t => {
+
+  t.plan(11)
+
+  var spec = [{
+    name: 'blocks',
+    features: [{
+      geometry: "POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))",
+      properties: {
+        hello: 'world',
+        is_tile: true,
+        feature_id: 1234,
+        score: 0.9993253156,
+      }
+    }]
+  }]
+
+  var enc = tileEncode(spec)
+  var tile = new vt.VectorTile(enc)
+
+  t.ok('blocks' in tile.layers)
+
+  var layer = tile.layers.blocks
+  t.equal(layer.length, 1)
+
+  var feature = layer.feature(0)
+  t.ok('properties' in feature)
+
+  var properties = feature.properties
+  var expectedProperties = spec[0].features[0].properties
+
+  t.ok('hello' in properties)
+  t.equal(properties.hello, expectedProperties.hello)
+
+  t.ok('is_tile' in properties)
+  t.equal(properties.is_tile, expectedProperties.is_tile)
+
+  t.ok('feature_id' in properties)
+  t.equal(properties.feature_id, expectedProperties.feature_id)
+
+  t.ok('score' in properties)
+  t.equal(properties.score, expectedProperties.score)
+
+})
+
 //
 // test('tile test', t => {
 //

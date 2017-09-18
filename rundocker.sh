453c39c08cd15265721fca00565f9d0a39cbf53a
George Lifchits <george.lifchits@gmail.com>
2017-09-17 21:23:13 -0400

    add simple command to run in Docker without mounting node_modules

--- rundocker.sh
+++ rundocker.sh
@@ -2,3 +2,4 @@ IMG=tileserver-backendnode
 
 docker build -t $IMG .
 docker run -it -v $(pwd):/app -p 5000:5000 $IMG bash
+# docker run -it -v $(pwd):/app -v /app/node_modules -p 5000:5000 $IMG bash
